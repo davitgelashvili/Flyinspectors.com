0809c684e608de238c1e092df77010ea34941a02
Abzhandadze23 <labjandadze23@gmail.com>
2024-11-15 23:50:32 +0400

    update

--- src/components/SendForm/Form.js
+++ src/components/SendForm/Form.js
@@ -136,80 +136,89 @@ import TextInput from "../UI/TextInput";
 import styles from "./form.module.scss";
 
 const Form = ({ value, setValue, uploadFile }) => {
-  const inputs = [
-    {
-      id: 32345,
-      name: "firstName",
-      type: "text",
-      value: value.firstName,
-      placeholder: "firstname",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg",
-    },
-    {
-      id: 1574,
-      name: "lastName",
-      type: "text",
-      value: value.lastName,
-      placeholder: "lastName",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg",
-    },
-    {
-      id: 46477,
-      name: "phone",
-      type: "text",
-      value: value.phone,
-      placeholder: "phone",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600390/Form%20icons/ifezno0nleyy8tyuclzg.svg",
-    },
-    {
-      id: 5703,
-      name: "email",
-      type: "text",
-      value: value.email,
-      placeholder: "email",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/nslmitrcambiai6wmfen.svg",
-    },
-    {
-      id: 34643,
-      name: "city",
-      type: "text",
-      value: value.city,
-      placeholder: "city",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/kuhub9sf2shcfwtecvy9.svg",
-    },
-    {
-      id: 753235,
-      name: "address",
-      type: "text",
-      value: value.address,
-      placeholder: "address",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/chfs24yjcgkltrezdbzn.svg",
-    },
-    {
-      id: 485845,
-      name: "flightNumber",
-      type: "text",
-      value: value.fightNumber,
-      placeholder: "fightNumber",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/mn0qzrqet0padkpm6rgj.svg",
-    },
-    {
-      id: 457853,
-      name: "date",
-      type: "text",
-      value: value.date,
-      placeholder: "date",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/ivmgutc8eckheli6at9q.svg",
-    },
-    {
-      id: 2542645,
-      name: "description",
-      type: "text",
-      value: value.description,
-      placeholder: "description",
-      icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/nzxyqpypgliouotsa6ie.svg",
-    },
-  ];
+    const inputs = [
+        {
+            id: 32345,
+            name: "firstName",
+            type: "text",
+            value: value.firstName,
+            placeholder: "firstname",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg"
+        },
+        {
+            id: 1574,
+            name: "lastName",
+            type: "text",
+            value: value.lastName,
+            placeholder: "lastName",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg"
+        },
+        {
+            id: 46477,
+            name: "phone",
+            type: "text",
+            value: value.phone,
+            placeholder: "phone",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600390/Form%20icons/ifezno0nleyy8tyuclzg.svg"
+        },
+        {
+            id: 5703,
+            name: "email",
+            type: "text",
+            value: value.email,
+            placeholder: "email",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/nslmitrcambiai6wmfen.svg"
+        },
+        {
+            id: 34643,
+            name: "city",
+            type: "text",
+            value: value.city,
+            placeholder: "city",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/kuhub9sf2shcfwtecvy9.svg"
+        },
+        {
+            id: 753235,
+            name: "address",
+            type: "text",
+            value: value.address,
+            placeholder: "address",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/chfs24yjcgkltrezdbzn.svg"
+        },
+        {
+            id: 36273,
+            name: "problem",
+            type: "text",
+            value: value.problem,
+            placeholder: "problem",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/sfew5nqowhazn00o76ho.svg"
+        },
+        {
+            id: 485845,
+            name: "flightNumber",
+            type: "text",
+            value: value.fightNumber,
+            placeholder: "fightNumber",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/mn0qzrqet0padkpm6rgj.svg"
+        },
+        {
+            id: 457853,
+            name: "date",
+            type: "text",
+            value: value.date,
+            placeholder: "date",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/ivmgutc8eckheli6at9q.svg"
+        },
+        {
+            id: 2542645,
+            name: "description",
+            type: "text",
+            value: value.description,
+            placeholder: "description",
+            icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/nzxyqpypgliouotsa6ie.svg"
+        },
+        
+    ]
 
   const handleChange = (e) => {
     setValue({ ...value, [e.target.name]: e.target.value });
