f43ec05c7b4172bdf678af91d1ca7513722693f0
Abzhandadze23 <labjandadze23@gmail.com>
2024-11-15 23:56:45 +0400

    update

--- src/components/SendForm/Form.js
+++ src/components/SendForm/Form.js
@@ -1,134 +1,3 @@
-// import { useEffect, useState } from "react"
-// import UploadWidget from "../UploadWidget/UploadWidget"
-// import TextInput from "../UI/TextInput"
-// import styles from './form.module.scss'
-
-
-// const Form = ({value, setValue, uploadFile}) => {
-//     const inputs = [
-//         {
-//             id: 32345,
-//             name: "firstName",
-//             type: "text",
-//             value: value.firstName,
-//             placeholder: "firstname",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg"
-//         },
-//         {
-//             id: 1574,
-//             name: "lastName",
-//             type: "text",
-//             value: value.lastName,
-//             placeholder: "lastName",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600392/Form%20icons/ijhlmpfbajgs0ypeymoy.svg"
-//         },
-//         {
-//             id: 46477,
-//             name: "phone",
-//             type: "text",
-//             value: value.phone,
-//             placeholder: "phone",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600390/Form%20icons/ifezno0nleyy8tyuclzg.svg"
-//         },
-//         {
-//             id: 5703,
-//             name: "email",
-//             type: "text",
-//             value: value.email,
-//             placeholder: "email",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/nslmitrcambiai6wmfen.svg"
-//         },
-//         {
-//             id: 34643,
-//             name: "city",
-//             type: "text",
-//             value: value.city,
-//             placeholder: "city",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/kuhub9sf2shcfwtecvy9.svg"
-//         },
-//         {
-//             id: 753235,
-//             name: "address",
-//             type: "text",
-//             value: value.address,
-//             placeholder: "address",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/chfs24yjcgkltrezdbzn.svg"
-//         },
-//         {
-//             id: 36273,
-//             name: "problem",
-//             type: "text",
-//             value: value.problem,
-//             placeholder: "problem",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/sfew5nqowhazn00o76ho.svg"
-//         },
-//         {
-//             id: 485845,
-//             name: "flightNumber",
-//             type: "text",
-//             value: value.fightNumber,
-//             placeholder: "fightNumber",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/mn0qzrqet0padkpm6rgj.svg"
-//         },
-//         {
-//             id: 457853,
-//             name: "date",
-//             type: "text",
-//             value: value.date,
-//             placeholder: "date",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/ivmgutc8eckheli6at9q.svg"
-//         },
-//         {
-//             id: 2542645,
-//             name: "description",
-//             type: "text",
-//             value: value.description,
-//             placeholder: "description",
-//             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/nzxyqpypgliouotsa6ie.svg"
-//         },
-        
-//     ]
-
-//     const handleChange = (e) =>{
-//         setValue({...value,[e.target.name]: e.target.value})
-//     }
-
-//     return (
-//         <div className={styles.bgColor}>
-//             <div className={styles.headerForm}>
-//             <h3>Fill Form</h3>
-//             <h3> Check the Status</h3>
-//             </div>
-//             <div className={styles.formDiv}>
-//         <form className={styles.formContainer}>
-//             {inputs.map((input)=>{
-//                 return (
-//                     <TextInput 
-//                         key={input.id}
-//                         type={input.type}
-//                         value={input.value}
-//                         placeholder={input.placeholder}
-//                         name={input.name}
-//                         icon={input.icon}
-//                         onChange={handleChange}
-//                         />
-//                 )
-//             })}
-//         </form>   
-//     </div>
-//     <div className="btnDiv">
-//         <UploadWidget value={value} valueName={"passportImage"} setValue={setValue}/>
-//         <UploadWidget value={value} valueName={"ticketImage"} setValue={setValue}/>
-//         <button onClick={(e) => uploadFile(e)}>Submit Form</button>
-//         {value.passportImage && <img src={value.passportImage} alt="" />}
-//     </div>
-//         </div>
-        
-//     )
-// }
-
-// export default Form
-
 
 import { useEffect, useState } from "react";
 import UploadWidget from "../UploadWidget/UploadWidget";
@@ -190,7 +59,7 @@ const Form = ({ value, setValue, uploadFile }) => {
             name: "problem",
             type: "text",
             value: value.problem,
-            placeholder: "problem",
+            placeholder: "Select problem",
             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/sfew5nqowhazn00o76ho.svg"
         },
         {
@@ -198,7 +67,7 @@ const Form = ({ value, setValue, uploadFile }) => {
             name: "flightNumber",
             type: "text",
             value: value.fightNumber,
-            placeholder: "fightNumber",
+            placeholder: "flightNumber",
             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600391/Form%20icons/mn0qzrqet0padkpm6rgj.svg"
         },
         {
@@ -211,10 +80,10 @@ const Form = ({ value, setValue, uploadFile }) => {
         },
         {
             id: 2542645,
-            name: "description",
+            name: "Select",
             type: "text",
             value: value.description,
-            placeholder: "description",
+            placeholder: "Select",
             icon: "https://res.cloudinary.com/dluqxr8lw/image/upload/v1731600393/Form%20icons/nzxyqpypgliouotsa6ie.svg"
         },
         
