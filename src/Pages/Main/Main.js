2397b3b8e457a6cd6cff3b3f84f9029abcfab5ea
Abzhandadze23 <labjandadze23@gmail.com>
2024-11-16 17:35:58 +0400

    section Four

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -65,6 +65,8 @@ function Main() {
           </p>
         </div>
       </div>
+
+      {/* es undat rom dinamiuri iyos da mere gadavcvalot. */}
       <div className={styles.sectionThree}>
         <div className={styles.headerOfSection}>
           <h3>
@@ -102,6 +104,23 @@ function Main() {
               we are not successful you pay nothing.
             </p>
           </div>
+        </div>
+      </div>
+      <div className={styles.sectionFour}>
+        <h1>here will be picture</h1>
+        <div>
+            <h3> WHY FLYINSPECTORS?</h3>
+            <p>
+                When struggling with flight-related problem, most of the passengers don't
+                even know they are eligible for compensation. let alone the resources they
+                need to determine if they qualify for it. You may need legal 
+                expertise and a lot of time to get familiar with the flight
+                regulations and your rights as an airline passenger.
+                Fortunately, there is easier solution, Flyinspectors are experts in this field. 
+                We can ease the process of getting the flight compensation you may qualify for.
+                And it's completely free for you, we only take fee of 25% in case of success. 
+                Are we not successful, you pay nothing.
+             </p>
         </div>
       </div>
     </main>
