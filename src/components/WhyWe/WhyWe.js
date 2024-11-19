c4cb58b1c59623d58036fc45754c4d98eecf08b5
Davit Gelashvili <datoogelashvili@gmail.com>
2024-11-19 20:05:14 +0400

    change style

--- src/components/WhyWe/WhyWe.js
+++ src/components/WhyWe/WhyWe.js
@@ -1,15 +1,16 @@
 import Item from "./Item.js";
 import styles from "./WhyWe.module.scss";
+import cover from './../../assetss/images/rb_2148.png'
 
 const WhyWe = () => {
   return (
     <div className={styles.services}>
       <div className="container">
         <div className="row">
-          <div className="col-4">
-            <h1>image</h1>
+          <div className="col-lg-4">
+            <img src={cover} alt="cover" className={styles.services__cover} />
           </div>
-          <div className="col-8">
+          <div className="col-lg-8">
             <Item />
           </div>
         </div>
