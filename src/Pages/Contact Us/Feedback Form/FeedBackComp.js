a03f3d9391d18d6c9405f37876c76e26ba30b050
Abzhandadze23 <labjandadze23@gmail.com>
2024-11-30 18:45:37 +0400

    margin top

--- src/Pages/Contact Us/Feedback Form/FeedBackComp.js
+++ src/Pages/Contact Us/Feedback Form/FeedBackComp.js
@@ -4,7 +4,7 @@ function FeedBackComp() {
   return (
 <div className={styles.mainDiv}>
     <div className="mb-5 container row">
-      <h3>FeedBack Form</h3>
+      <h3 className="mt-5">FeedBack Form</h3>
       <div className="col-lg-6">
         <FeedBack />
       </div>
