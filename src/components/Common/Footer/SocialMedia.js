df1f5fd2e80699e9595b43db6a5cfa4cacab0bc7
nikkcartwright13 <125912421+nikkcartwright13@users.noreply.github.com>
2024-12-05 00:01:58 +0400

    update footer

--- src/components/Common/Footer/SocialMedia.js
+++ src/components/Common/Footer/SocialMedia.js
@@ -7,32 +7,34 @@ import Gmail from "../../Images/Gmail.png";
 import Tiktok from "../../Images/Tiktok.png";
 
 const SocialMedia = () => {
+
+  
   return (
     <div className={styles.socmedia}>
       <div className={styles.socmedia__title}>Social Media</div>
       <ul className={`${styles.socmedia__list} d-flex align-items-center`}>
         <li className={styles["socmedia__list--item"]} >
-          <a href="#" className={styles["socmedia__list--link"]}>
+          <a href="https://www.facebook.com/FlyinspectorsEng" className={styles["socmedia__list--link"]}>
             <img src={Facebook} alt="Facebook" />
           </a>
         </li>
         <li className="nav-item ">
-          <a href="#" className="nav-link p-0 text-body-secondary">
+          <a href="https://www.instagram.com/flyinspectors/" className="nav-link p-0 text-body-secondary">
             <img src={Instagram} alt="Instagram" />
           </a>
         </li>
         <li className="nav-item ">
-          <a href="#" className="nav-link p-0 text-body-secondary">
+          <a href="https://Wa.me/+995593000394?text=I'm%20interested" className="nav-link p-0 text-body-secondary">
             <img src={Whatsapp} alt="Whatsapp" />
           </a>
         </li>
         <li className="nav-item ">
-          <a href="#" className="nav-link p-0 text-body-secondary">
+          <a href="mailto:team@flyinspectors.com" className="nav-link p-0 text-body-secondary">
             <img src={Gmail} alt="Gmail" />
           </a>
         </li>
         <li className="nav-item ">
-          <a href="#" className="nav-link p-0 text-body-secondary">
+          <a href="https://www.tiktok.com/@flyinspectors" className="nav-link p-0 text-body-secondary">
             <img src={Tiktok} alt="Tiktok" />
           </a>
         </li>
