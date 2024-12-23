88b9bf84e8d7e43ceffa6cae4e5d5650ca19c0e7
Davit Gelashvili <datoogelashvili@gmail.com>
2024-12-24 00:09:17 +0400

    finish

--- src/components/WhyWe/WhyWe.js
+++ src/components/WhyWe/WhyWe.js
@@ -1,6 +1,7 @@
 import Item from "./Item.js";
 import styles from "./WhyWe.module.scss";
 import cover from './../../assetss/images/rb_2148.png'
+import SectionCover from "../SectionTextAndCover/SectionCover.js";
 
 const WhyWe = () => {
   return (
@@ -8,7 +9,8 @@ const WhyWe = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
-            <img src={cover} alt="cover" className={styles.services__cover} />
+            <SectionCover image={cover}/>
+            {/* <img src={cover} alt="cover" className={styles.services__cover} /> */}
           </div>
           <div className="col-lg-8">
             <Item />
